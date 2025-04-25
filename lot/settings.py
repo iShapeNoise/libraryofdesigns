97ef9a55c714bea7a82795541d090896ad1a3d90
Jens Meisner <yenz4xyz@gmail.com>
2025-04-25 16:43:02 +0000

    add project files and deploy.sh

--- lot/settings.py
+++ lot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 from decouple import Csv 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'PORT': config('DB_PORT'),
-        'HOST': config('DB_HOST')
+        'HOST': config('DB_HOST'),
     }
 }
 
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
---
+++ scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# activate virtual environment
+source ~/pylot/bin/activate
+
+# cd into project folder
+cd ~/lot
+
+# pull the latest code base
+git pull
+
+# install app dependencies
+pip install -r requirements.txt
+
+# run database migration
+python manage.py migrage
+
+# run the collect static command
+python manage.py collectstatic --no-input
+
+# put all other commands that required
+
+# deactivate
+deactivate
+
+# reload nginx
+sudo systemctl reload nginx
+
+# restart gunicorn
+sudo systemctl restart gunicorn
+
